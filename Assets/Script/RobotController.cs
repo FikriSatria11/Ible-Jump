63c7a63b471a7697fff16691840580a7d39783b2
Computer Science <compsci@labrpl-16.localdomain>
2019-12-09 10:46:08 +0800

    final commit

---
+++ Assets/Script/RobotController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+	//This will be our maximum speed as we will always be multiplying by 1
+	public float maxSpeed = 2f;
+	//a boolean value to represent whether we are facing left or not
+	bool facingLeft = true;
+	//a value to represent our Animator
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
+	// Use this for initialization
+	void Start () {
+		//set anim to our animator
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+
+		anim.SetBool ("Ground", grounded);
+
+
+		float move = Input.GetAxis ("Horizontal");//Gives us of one if we are moving via the arrow keys
+		//move our Players rigidbody
+		GetComponent<Rigidbody2D>().velocity = new Vector3 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);	
+		//set our speed
+		anim.SetFloat ("Speed",Mathf.Abs (move));
+		//if we are moving left but not facing left flip, and vice versa
+		if (move < 0 && !facingLeft) {
+			
+			Flip ();
+		} else if (move > 0 && facingLeft) {
+			Flip ();
+		}
+	}
+
+	void Update(){
+		if(grounded && Input.GetKeyDown (KeyCode.Space)){
+			anim.SetBool("Ground",false);
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0,jumpForce));
+		}
+		
+		
+	}
+
+	//flip if needed
+	void Flip(){
+		facingLeft = !facingLeft;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Assets/Script/pancam.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class pancam : MonoBehaviour
+{
+    float ydir = 0f;
+	public GameObject player;
+	//for our GUIText object and our score
+	public TextMeshPro gui;
+	float playerScore = 0;
+
+	//this function updates our guitext object
+	void OnGUI(){
+		gui.GetComponent<TextMeshPro>().text = "Score: " + ((int)(playerScore * 10)).ToString ();
+	}
+	//this is generic function we can call to increase the score by an amount
+	public void increaseScore(int amount){
+		playerScore += amount;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//check that player exists and then proceed. otherwise we get an error when player dies
+		if (player) {
+
+			//if player has passed the x position of -1 then start moving camera forward with a randomish Y position
+			if (player.transform.position.x > -1) {
+
+				//update our score every tick of the clock
+				playerScore += Time.deltaTime;
+				
+				float randy = 0f;
+				randy = Random.Range (0f, 100f);
+				if (randy < 20) {
+					ydir = ydir + .005f;
+				} else if (randy > 20 && randy < 40) {
+					ydir = ydir - .005f;
+				} else if (randy > 80) {
+					ydir = 0f;
+				}
+				transform.position = new Vector3 (transform.position.x + 0.03f, transform.position.y + ydir, -10);
+			}
+		}
+	}
+}
